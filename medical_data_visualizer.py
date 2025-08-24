9241376a6d4745c632923caa4cfc44396ce7f13d
Enzo Frediani <enzofrediani@usp.br>
2025-08-24 01:21:27 +0000

    solucao

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -15,24 +15,14 @@ df['cholesterol'] = np.where(df['cholesterol'] == 1, 0, 1)
 
 # 4
 def draw_cat_plot():
-    # 5
-    df_cat = pd.melt(df, id_vars=['id', 'sex', 'height', 'weight', 'ap_hi', 'ap_lo', 'cardio'], value_vars = ['cholesterol', 'gluc', 'smoke', 'active', 'overweight'])
-
-    # 6
-    df_cat = df_cat.sort_values(by='cardio')
-
-    
-
     # 7
     df_cat = pd.melt(df, id_vars=['cardio'], value_vars = ['cholesterol', 'gluc', 'smoke', 'active', 'overweight', 'alco'])
     df_cat = (df_cat.groupby('cardio').value_counts().reset_index(name='total'))
 
     # 8
     fig = sns.catplot(data=df_cat, x="variable", y="total", hue="value", col="cardio", kind="bar", order=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
-
-    # 9
     fig.savefig('catplot.png')
-    return fig
+    return fig.fig
 
 
 # 10
